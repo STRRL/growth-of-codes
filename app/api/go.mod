ebf8348b49b4b278e3245ef364904454f2f1ab9d
STRRL <str_ruiling@outlook.com>
2022-02-16 22:33:27 +0800

    chore: list analyzed repositories
    Signed-off-by: STRRL <str_ruiling@outlook.com>

--- app/api/go.mod
+++ app/api/go.mod
@@ -1,3 +1,15 @@
 module github.com/STRRL/growth-of-codes/app/api
 
 go 1.17
+
+require (
+	github.com/STRRL/growth-of-codes v0.0.2-0.20220216140725-ad977b3d60df
+	gorm.io/driver/mysql v1.2.3
+	gorm.io/gorm v1.22.5
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+)
---
+++ app/api/repository/list.go
@@ -0,0 +1,50 @@
+package repository
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/STRRL/growth-of-codes/pkg/persistent/entity"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"net/http"
+	"os"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	dsn := os.Getenv("MYSQL_DSN")
+	if len(dsn) == 0 {
+		fmt.Fprintf(w, "MYSQL_DSN is not set")
+	}
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
+	if err != nil {
+		_, _ = fmt.Fprintf(w, "gorm.Open error: %v", err)
+		return
+	}
+	repositories, err := listRepositories(db)
+	if err != nil {
+		_, _ = fmt.Fprintf(w, "listRepositories error: %v", err)
+		return
+	}
+	bytes, err := json.Marshal(repositories)
+	if err != nil {
+		_, _ = fmt.Fprintf(w, "json.Marshal error: %v", err)
+		return
+	}
+	_, _ = w.Write(bytes)
+}
+
+func listRepositories(db *gorm.DB) ([]string, error) {
+	err := db.AutoMigrate(&entity.FileComplexitySnapshot{})
+	if err != nil {
+		return nil, err
+	}
+	var result []string
+	query := db.Raw("select distinct project from file_complexity_snapshots;").Scan(&result)
+	if query.Error != nil {
+		return nil, query.Error
+	}
+	return result, nil
+}
